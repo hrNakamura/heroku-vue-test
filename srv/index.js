461eb126552fccfa868a5c8d5e73e76fbcfd5af6
hiroshi.nh.nakamura <hiroshi.nh.nakamura@jp.ricoh.com>
2019-10-09 10:48:05 +0900

    add client ip filter

--- srv/index.js
+++ srv/index.js
@@ -1,13 +1,23 @@
 import express from 'express';
+import ip from 'ip';
 // import socketIO from "socket.io";
 
 export default (app, http) => {
   const port = process.env.PORT || 3000;
   app.set('port', port);
 
-  var ipfilter = require('express-ipfilter').IpFilter;
-  var ips = [['10.60.183.1', '10.60.183.255']];
-  app.use(ipfilter(ips, { mode: 'allow' }));
+  var gateway = '10.60.183.0';
+  var mask = '255.255.255.0';
+  app.use((req, res, next ) => {
+    var reqIP = req.connection.remoteAddress;
+    var reqGateway = ip.mask(reqIP, mask);
+    if(ip.isEqual(gateway, reqGateway)){
+      next();
+    }else{
+      res.send('not allowed :' + reqIP + ', ' + reqGateway);
+    }
+  });
+
   app.use(express.json());
   //
   app.get('/foo', (req, res) => {
