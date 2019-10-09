50951c9e62f0ad45a3c0f2fe3d5775c7300bfe8e
hiroshi.nh.nakamura <hiroshi.nh.nakamura@jp.ricoh.com>
2019-10-09 10:53:53 +0900

    remove ip filter

--- srv/index.js
+++ srv/index.js
@@ -1,22 +1,22 @@
 import express from 'express';
-import ip from 'ip';
+// import ip from 'ip';
 // import socketIO from "socket.io";
 
-export default (app, http) => {
+export default (app) => {
   const port = process.env.PORT || 3000;
   app.set('port', port);
 
-  var gateway = '10.60.183.0';
-  var mask = '255.255.255.0';
-  app.use((req, res, next ) => {
-    var reqIP = req.connection.remoteAddress;
-    var reqGateway = ip.mask(reqIP, mask);
-    if(ip.isEqual(gateway, reqGateway)){
-      next();
-    }else{
-      res.send('not allowed :' + reqIP + ', ' + reqGateway);
-    }
-  });
+  // var gateway = '10.60.183.0';
+  // var mask = '255.255.255.0';
+  // app.use((req, res, next ) => {
+  //   var reqIP = req.connection.remoteAddress;
+  //   var reqGateway = ip.mask(reqIP, mask);
+  //   if(ip.isEqual(gateway, reqGateway)){
+  //     next();
+  //   }else{
+  //     res.send('not allowed :' + reqIP + ', ' + reqGateway);
+  //   }
+  // });
 
   app.use(express.json());
   //
